03404b74e94e23d34ae1041ee93adfe7f630bf51
miran <kcrt1020@naver.com>
2025-04-03 04:22:57 +0900

    db

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,13 @@ trap 'echo "❌ 에러 발생. 배포 중단됨."' ERR
 
 BRANCH=${1:-main}
 
-echo "📥 Pulling latest code from branch: $BRANCH"
+echo "📥 강제 초기화 후 최신 코드 가져오는 중..."
+git reset --hard HEAD
 git pull origin $BRANCH
 git submodule sync
 git submodule update --remote --merge
 
-echo "🐳 Restarting Docker..."
+echo "🐳 Docker 재시작 중..."
 docker compose down --remove-orphans
 docker compose build --no-cache
 docker compose up -d
